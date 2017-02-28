48b9d0c2cb248706afce6e159ac913f7bd05f7f6
Ben Pepper <bfpepper87@gmail.com>
2017-02-27 18:08:25 -0700

    adds index view logic.

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,7 +1,7 @@
 class SiteController < ApplicationController
 
   def index
-    @links = Link.where(created_at: (Time.now - 1.day)..Time.now)
+    @links = Link.where(created_at: (Time.now - 1.day)..Time.now).group(:url).order("count_id DESC").count("id")
   end
 
 end
--- config/application.rb
+++ config/application.rb
@@ -21,11 +21,11 @@ module HotReads
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
-      end
-    end
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    # config.i18n.default_locale = :de
+
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
   end
 end
